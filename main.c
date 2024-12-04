2834e10356b62ad11da88f851b9d32f4c8c70177
Thiago F. <150091259+thigoai@users.noreply.github.com>
2024-12-03 23:55:30 -0300

    Update main.c

--- main.c
+++ main.c
@@ -11,7 +11,6 @@ typedef struct arvore_avl {
 
 } arvore_avl;
 
-// Função para calcular a altura de uma árvore
 int calcular_altura(arvore_avl* arv) {
     if (arv == NULL) {
         return 0;
@@ -23,25 +22,21 @@ int calcular_altura(arvore_avl* arv) {
 
 // Função de rotação à esquerda
 arvore_avl* rotacao_esquerda(arvore_avl* raiz) {
-    arvore_avl *novo_raiz = raiz->dir;            // O nó à direita de 'raiz' se torna a nova raiz
-    arvore_avl *subarvore_direita = novo_raiz->esq; // O nó à esquerda de 'novo_raiz' é temporariamente 'subarvore_direita'
+    arvore_avl *novo_raiz = raiz->dir;           
+    arvore_avl *subarvore_direita = novo_raiz->esq; 
 
-    // Realizando a rotação à esquerda
     novo_raiz->esq = raiz;
     raiz->dir = subarvore_direita;
 
-    // Atualizando as alturas após a rotação
     raiz->altura_esq = calcular_altura(raiz->esq);
     raiz->altura_dir = calcular_altura(raiz->dir);
 
     novo_raiz->altura_esq = calcular_altura(novo_raiz->esq);
     novo_raiz->altura_dir = calcular_altura(novo_raiz->dir);
 
-    // Retornando a nova raiz
     return novo_raiz;
 }
 
-// Função para criar um nó da árvore
 arvore_avl* criar_novo_no(int valor) {
     arvore_avl* novo_no = (arvore_avl*) malloc(sizeof(arvore_avl));
     novo_no->valor = valor;
@@ -51,15 +46,12 @@ arvore_avl* criar_novo_no(int valor) {
 }
 
 int main() {
-    // Exemplo de uso
     arvore_avl* raiz = criar_novo_no(10);
     raiz->dir = criar_novo_no(20);
     raiz->dir->dir = criar_novo_no(30);
 
-    // Realiza a rotação à esquerda
     raiz = rotacao_esquerda(raiz);
 
-    // Exibe o valor da nova raiz após a rotação
     printf("Novo valor da raiz: %d\n", raiz->valor);
 
     return 0;
