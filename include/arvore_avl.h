bc2c0dfc69a29b301a6e92f81c3bf022ac48074b
Thiago F. <150091259+thigoai@users.noreply.github.com>
2024-12-09 23:32:04 -0300

    Update arvore_avl.h

--- include/arvore_avl.h
+++ include/arvore_avl.h
@@ -1,19 +1,26 @@
 #pragma once
 
 typedef struct arvore_avl {
-    int valor;
-    int altura_dir;
-    int altura_esq;
+  int valor;
+  int altura;
+  int altura_dir;
+  int altura_esq;
 
-    struct arvore_avl *esq;
-    struct arvore_avl *dir;
+  struct arvore_avl *esq;
+  struct arvore_avl *dir;
 
 } arvore_avl;
 
-int calcular_altura(arvore_avl* arv);
+int calcular_altura(arvore_avl *arv);
 
-arvore_avl* rotacao_esquerda(arvore_avl* raiz);
+arvore_avl *rotacao_esquerda(arvore_avl *raiz);
 
-arvore_avl* rotacao_direita(arvore_avl* raiz);
+arvore_avl *rotacao_direita(arvore_avl *raiz);
 
-arvore_avl* criar_novo_no(int valor);
+arvore_avl *criar_novo_no(int valor);
+
+arvore_avl *balancear_arvore(arvore_avl *raiz);
+
+arvore_avl *inserir_no(arvore_avl *raiz, int valor);
+
+arvore_avl *remover_no(arvore_avl *raiz, int valor);
