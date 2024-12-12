47ca761cf601521d66b89a01fcd7d4d96fd6d6f7
Thiago F. <150091259+thigoai@users.noreply.github.com>
2024-12-12 11:45:55 -0300

    Update arvore_avl.c

--- src/arvore_avl.c
+++ src/arvore_avl.c
@@ -12,6 +12,36 @@ int calcular_altura(arvore_avl *arv) {
   return (altura_esq > altura_dir ? altura_esq : altura_dir) + 1;
 }
 
+void mostrarEmOrdem(arvore_avl *arv){
+
+  if(arv != NULL){
+    mostrarEmOrdem(arv->esq);
+    printf("  %d", arv->valor);
+    mostrarEmOrdem(arv->dir); 
+  }
+
+}
+
+void mostrarPreOrdem(arvore_avl *arv){
+
+  if(arv != NULL){
+    printf("  %d", arv->valor);
+    mostrarPreOrdem(arv->esq);
+    mostrarPreOrdem(arv->dir);
+  }
+
+}
+
+void mostrarPosOrdem(arvore_avl *arv){
+
+  if(arv != NULL){
+    mostrarPosOrdem(arv->esq);
+    mostrarPosOrdem(arv->dir);
+    printf("  %d", arv->valor);
+  }
+
+}
+
 arvore_avl *rotacao_esquerda(arvore_avl *raiz) {
   arvore_avl *novo_raiz = raiz->dir;
   arvore_avl *subarvore_dir = novo_raiz->esq;
@@ -98,7 +128,7 @@ arvore_avl *inserir_no(arvore_avl *raiz, int valor) {
 }
 
 arvore_avl *buscar(arvore_avl *raiz, int valor) {
-  
+
     if (raiz == NULL) {
         return 0;
     }
